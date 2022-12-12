13cbfc82c7be2e66bcb1029b5ad9dd30778e3c16
gogo <90778453+georgeayman97@users.noreply.github.com>
2022-12-12 17:24:48 +0200

    voyages export

--- app/Exports/VoyageExport.php
+++ app/Exports/VoyageExport.php
@@ -11,17 +11,15 @@ class VoyageExport implements FromCollection,WithHeadings
     public function headings(): array
     {
         return [
-            "REF NO",
-            "AGENT",
-            "CUSTOMER",
-            "VALIDITY FROM" ,
-            "VALIDITY TO",
-            "EQUIPMENT TYPE",
-            "PLACE OF ACCEPTENCE",
-            "PLACE OF DELIVERY" ,
-            "LOAD PORT",
-            "DISCHARGE PORT",
-            "STATUS",
+            "Vessel Code",
+            "Vessel Name",
+            "Voyage No",
+            "Leg" ,
+            "PORT",
+            "ETA",
+            "ETD",
+            "Terminal Name" ,
+            "Road NO",
         ];
     }
     
@@ -29,19 +27,25 @@ class VoyageExport implements FromCollection,WithHeadings
     public function collection()
     {
        
-        $quotations = session('quotations');
-        //$quotations = Quotation::all();
-
-        foreach($quotations as $quotation){
-            $quotation->agent_id = optional($quotation->agent)->name;
-            $quotation->customer_id = optional($quotation->customer)->name;
-            $quotation->equipment_type_id = optional($quotation->equipmentsType)->name;
-            $quotation->place_of_acceptence_id = optional($quotation->placeOfAcceptence)->name;
-            $quotation->place_of_delivery_id = optional($quotation->placeOfDelivery)->name;
-            $quotation->load_port_id = optional($quotation->loadPort)->name;
-            $quotation->discharge_port_id = optional($quotation->dischargePort)->name;
+        $voyages = session('voyages');
+        $exportVoyages = collect();
+        foreach($voyages as $voyage){
+            foreach($voyage->voyagePorts as $voyagePort){
+                $tempCollection = collect([
+                    'code' => optional($voyage->vessel)->code,
+                    'name' => optional($voyage->vessel)->name,
+                    'voyage_no'=> $voyage->voyage_no,
+                    'leg'=> optional($voyage->leg)->name,
+                    'port'=> optional($voyagePort->port)->name,
+                    'eta'=> $voyagePort->eta,
+                    'etd'=> $voyagePort->etd,
+                    'terminal'=> optional($voyagePort->terminal)->name,
+                    'road_no'=> $voyagePort->road_no,
+                ]);
+                $exportVoyages->add($tempCollection);
+            }
         }
         
-        return $quotations;
+        return $exportVoyages;
     }    
 }
