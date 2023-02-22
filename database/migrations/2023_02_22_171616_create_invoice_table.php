bcfc8f35cf71cddeabd66859a7d5b4acac7f3ae9
gogo <90778453+georgeayman97@users.noreply.github.com>
2023-02-22 19:55:07 +0200

    migrations of invoice

---
+++ database/migrations/2023_02_22_171616_create_invoice_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_no','255')->nullable();
+            $table->string('customer','255')->nullable();
+            $table->unsignedInteger('bldraft_id');
+            $table->date('date')->nullable();
+            $table->date('departure')->nullable();
+            $table->string('invoice_kind','255')->nullable();
+            $table->string('invoice_status','255')->nullable();
+            $table->string('total_amount_in_words','255')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice');
+    }
+}
