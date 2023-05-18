b4329aa3d2e74c5cc4ef5ef630fb0372b2d24b00
george ayman <george.ayman97@gmail.com>
2023-05-18 15:09:33 +0300

    edits 18/5

--- app/Exports/CustomerStatementsExport.php
+++ app/Exports/CustomerStatementsExport.php
@@ -18,8 +18,8 @@ class CustomerStatementsExport implements FromCollection,WithHeadings
             "Amount EGP",
             "BL NO",
             "Receipt No",
-            "Receipt Amount EGP",
             "Receipt Amount USD",
+            "Receipt Amount EGP",
             "Balance USD",
             "Balance EGP",
             "Vessel / Voyage",
@@ -34,6 +34,12 @@ class CustomerStatementsExport implements FromCollection,WithHeadings
         $exportinvoices = collect();
 
         foreach($customers  ?? [] as $customer){
+            $total_invoice_amount = 0;
+            $total_invoice_amount_egp = 0;
+            $total_receipt_amount_usd = 0;
+            $total_receipt_amount_egp = 0;
+            $total_balance_usd = 0;
+            $total_balance_egp = 0;
             foreach($customer->invoices as $invoice){
                 $totalusd = null;
                 $totalegp = null;
@@ -53,6 +59,7 @@ class CustomerStatementsExport implements FromCollection,WithHeadings
                 }
                     $blanceEgp = $totalreceipt - $totalegp;
                     $blanceUSD = $totalreceipt - $totalusd;
+                    dd($blanceEgp,$blanceUSD);
                 if($invoice->receipts->count() != 0){
                     foreach($invoice->receipts as $receipt){
                         $tempCollection = collect([
@@ -60,14 +67,14 @@ class CustomerStatementsExport implements FromCollection,WithHeadings
                             'type' => optional($invoice)->type,
                             'invoice_no' => optional($invoice)->invoice_no,
                             'date' => optional($invoice)->date,
-                            'invoice_amount' => $invoice->add_egp != 'onlyegp' ? $totalusd : 0,
-                            'invoice_amount_egp' => ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? $totalegp : 0,
+                            'invoice_amount' => $invoice->add_egp != 'onlyegp' ? $totalusd : '0',
+                            'invoice_amount_egp' => ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? $totalegp : '0',
                             'bl no' => $invoice->bldraft_id == 0 ?  'Customize' : optional($invoice->bldraft)->ref_no,
                             'receipts' => $receipt->receipt_no,
-                            'receipt_amount_egp' => ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? $totalreceipt:0,
-                            'receipt_amount_usd' => $invoice->add_egp != 'onlyegp' ? $totalreceipt : 0,
-                            'balance_usd' =>  $invoice->add_egp != 'onlyegp' ? $blanceUSD : 0,
-                            'balance_egp' =>  ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? $blanceEgp : 0,
+                            'receipt_amount_usd' => $invoice->add_egp != 'onlyegp' ? $totalreceipt : '0',
+                            'receipt_amount_egp' => ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? $totalreceipt:'0',
+                            'balance_usd' =>  $invoice->add_egp != 'onlyegp' ? ($blanceUSD == 0) ?? '0' : '0',
+                            'balance_egp' =>  ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? ($blanceEgp == 0)??'0' : '0',
                             'vessel-voyage' => ($invoice->bldraft_id == 0 ? optional(optional($invoice->voyage)->vessel)->name : optional($invoice->bldraft->voyage->vessel)->name) .' / '. ($invoice->bldraft_id == 0 ? optional($invoice->voyage)->voyage_no : optional($invoice->bldraft->voyage)->voyage_no),
                             'created_at' => $receipt->created_at,
                         ]);
@@ -80,14 +87,14 @@ class CustomerStatementsExport implements FromCollection,WithHeadings
                         'type' => optional($invoice)->type,
                         'invoice_no' => optional($invoice)->invoice_no,
                         'date' => optional($invoice)->date,
-                        'invoice_amount' => $invoice->add_egp != 'onlyegp' ? $totalusd : 0,
-                        'invoice_amount_egp' => ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? $totalegp : 0,
+                        'invoice_amount' => $invoice->add_egp != 'onlyegp' ? $totalusd : '0',
+                        'invoice_amount_egp' => ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? $totalegp : '0',
                         'bl no' => $invoice->bldraft_id == 0 ?  'Customize' : optional($invoice->bldraft)->ref_no,
                         'receipts' => '',
-                        'receipt_amount_egp' => ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? $totalreceipt:0,
-                        'receipt_amount_usd' => $invoice->add_egp != 'onlyegp' ? $totalreceipt : 0,
-                        'balance_usd' =>  $invoice->add_egp != 'onlyegp' ? $blanceUSD : 0,
-                        'balance_egp' =>  ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? $blanceEgp : 0,
+                        'receipt_amount_usd' => $invoice->add_egp != 'onlyegp' ? $totalreceipt : '0',
+                        'receipt_amount_egp' => ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? $totalreceipt:'0',
+                        'balance_usd' =>  $invoice->add_egp != 'onlyegp' ? ($blanceUSD == 0) ?? '0' : '0',
+                        'balance_egp' =>  ($invoice->add_egp == 'true' || $invoice->add_egp == 'onlyegp') ? ($blanceEgp == 0)??'0' : '0',
                         'vessel-voyage' => ($invoice->bldraft_id == 0 ? optional(optional($invoice->voyage)->vessel)->name : optional($invoice->bldraft->voyage->vessel)->name) .' / '. ($invoice->bldraft_id == 0 ? optional($invoice->voyage)->voyage_no : optional($invoice->bldraft->voyage)->voyage_no),
                         'created_at' => ''
                     ]);
@@ -103,14 +110,14 @@ class CustomerStatementsExport implements FromCollection,WithHeadings
                     'type' => "Credit Note",
                     'invoice_no' => optional($creditNote)->credit_no,
                     'date' => optional($creditNote)->created_at,
-                    'invoice_amount' => $creditNote->currency == "credit_usd" ? optional($creditNote)->total_amount : 0,
-                    'invoice_amount_egp' => $creditNote->currency == "credit_egp" ? optional($creditNote)->total_amount : 0,
+                    'invoice_amount' => $creditNote->currency == "credit_usd" ? optional($creditNote)->total_amount : '0',
+                    'invoice_amount_egp' => $creditNote->currency == "credit_egp" ? optional($creditNote)->total_amount : '0',
                     'bl no' => $creditNote->bl_no??'',
                     'receipts' => '',
-                    'receipt_amount_egp' => 0,
-                    'receipt_amount_usd' => 0,
-                    'balance_usd' =>  0,
-                    'balance_egp' =>  0,
+                    'receipt_amount_usd' => '0',
+                    'receipt_amount_egp' => '0',
+                    'balance_usd' =>  '0',
+                    'balance_egp' =>  '0',
                     'vessel-voyage' => '',
                     'created_at' => '',
                 ]);
@@ -124,14 +131,14 @@ class CustomerStatementsExport implements FromCollection,WithHeadings
                     'type' => "Refund",
                     'invoice_no' => optional($refund)->receipt_no,
                     'date' => optional($refund)->created_at,
-                    'invoice_amount' => $refund->status == "refund_usd" ? '-'.optional($refund)->paid : 0,
-                    'invoice_amount_egp' => $refund->status == "refund_egp" ? '-'.optional($refund)->paid : 0,
+                    'invoice_amount' => $refund->status == "refund_usd" ? '-'.optional($refund)->paid : '0',
+                    'invoice_amount_egp' => $refund->status == "refund_egp" ? '-'.optional($refund)->paid : '0',
                     'bl no' => '',
                     'receipts' => '',
-                    'receipt_amount_egp' => 0,
-                    'receipt_amount_usd' => 0,
-                    'balance_usd' =>  0,
-                    'balance_egp' =>  0,
+                    'receipt_amount_usd' => '0',
+                    'receipt_amount_egp' => '0',
+                    'balance_usd' =>  '0',
+                    'balance_egp' =>  '0',
                     'vessel-voyage' => '',
                     'created_at' => '',
                 ]);
