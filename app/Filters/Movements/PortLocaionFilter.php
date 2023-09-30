c0ffdd005c51cd44711a0fbabb9646d34e1545e1
unknown <mfadl887@gmail.com>
2023-09-30 14:25:43 +0300

    edits

--- app/Filters/Movements/PortLocaionFilter.php
+++ app/Filters/Movements/PortLocaionFilter.php
@@ -6,6 +6,6 @@ use App\Filters\AbstractBasicFilter;
 class PortLocaionFilter extends AbstractBasicFilter{
     public function filter($value)
     {
-        return $this->builder->Where('port_location_id',$value);
+        return $this->builder->whereIn('port_location_id',$value);
     }
 }
