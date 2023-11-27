bd1aa9b8f42ea603faafa1d2a519766c306bedf5
Mando266 <72440319+Mando266@users.noreply.github.com>
2023-11-27 13:10:36 +0200

    Update DetentionController.php

--- app/Http/Controllers/Containers/DetentionController.php
+++ app/Http/Controllers/Containers/DetentionController.php
@@ -2,150 +2,120 @@
 
 namespace App\Http\Controllers\Containers;
 
+use App\Filters\Containers\ContainersIndexFilter;
 use App\Http\Controllers\Controller;
+use App\Models\Containers\Bound;
+use App\Models\Containers\DemuragePeriodsSlabs;
 use App\Models\Containers\Demurrage;
-use App\Models\Containers\Movements;
 use App\Models\Containers\Period;
-use App\Models\Master\Containers;
+use App\Models\Containers\Triff;
+use App\Models\Master\ContainerStatus;
 use App\Models\Master\ContainersTypes;
+use App\Models\Master\Country;
+use App\Models\Master\Currency;
+use App\Models\Master\Ports;
+use App\Models\Master\Terminals;
+use App\TariffType;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class DetentionController extends Controller
+class DemurageController extends Controller
 {
-    /**
-     * 
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
-    }
+        $this->authorize(__FUNCTION__, Demurrage::class);
 
-    public function showDetentionView()
-    {
-        $demurrages = Demurrage::where('company_id',Auth::user()->company_id)->where('is_storge','Detention')->get();
-        $movementsBlNo = Movements::where('company_id',Auth::user()->company_id)->select('bl_no')->distinct()->get()->pluck('bl_no');
-        $movements = [];
-        //dd($demurrages);
+        $demurrage = Demurrage::where('company_id', Auth::user()->company_id)->get();
+        $demurrages = Demurrage::where('company_id', Auth::user()->company_id)->filter(new ContainersIndexFilter(request()))->get();
+        $countries = Country::orderBy('name')->get();
 
-        return view('containers.detention.detentionView',[
+        return view('containers.demurrage.index',[
+            'countries'=>$countries,
             'items'=>$demurrages,
-            'movementsBlNo'=>$movementsBlNo,
-            'movements' =>$movements,
-            'periods'=>[],
-        ]);
-    }
-    
-    public function calculateDetention(Request $request)
-    {
-        $period = Period::where('demurrage_id',$request->Triff_id)->select('rate','number_off_dayes','period')->get();
-        $movementsBlNo = Movements::where('company_id',Auth::user()->company_id)->select('bl_no')->distinct()->get()->pluck('bl_no');
-        //dd($movementsBlNo);
-        $demurrage = Demurrage::where('id',$request->Triff_id)->get();
-        $demurrages = Demurrage::where('company_id',Auth::user()->company_id)->get();
-        $movements = Movements::where('company_id',Auth::user()->company_id)->where('bl_no',$request->bl_no)->with('movementcode','container')->get();
-        $period = Period::where('demurrage_id',$request->Triff_id)->select('rate','number_off_dayes','period')->get();
-        $periodtimeTotal = 0;
-        foreach($period as $per){
-            if($per->period != "Thereafter")
-            $periodtimeTotal += $per->number_off_dayes;
-        }
-
-        return view('containers.detention.detentionView',[
-            'items'=>$demurrages,
-            'movementsBlNo'=>$movementsBlNo,
-            'movements'=> $movements,
-            'periods'=>$period,
-            'demurrage' => $demurrage,
-            'periodtimeTotal'=>$periodtimeTotal,
+            'demurrage'=>$demurrage,
         ]);
     }
 
-    public function showTriffSelectWithBlno($id,$detention,$dchfDate,$rcvcDate = null)
-    {           
-        $movement = Movements::find($id);
-        $containerType  = ContainersTypes::where('id',$movement->container_type_id)->pluck('name')->first();
-        $demurrages = Demurrage::where('company_id',Auth::user()->company_id)->get();
-        
-        $container_no = Containers::where('id',$movement->container_id)->pluck('code')->first();
-
-        return view('containers.detention.index',[
-            'containerType'=>$containerType,
-            'container_no'=>$container_no,
-            'dchfDate'=>$dchfDate,
-            'rcvcDate'=>$rcvcDate,
-            'periods'=>[],
-            'movement'=>$movement,
-            'items'=>$demurrages,
-            'detention' => $detention
-        ]);
-    }
-
-    
-    public function showDetention(Request $request)
-    {
-        $movement = Movements::where('id',$request->movement_id)->with('movementcode')->first();
-        $container_no = Containers::where('id',$movement->container_id)->pluck('code')->first();
-        $detention = $request->detention;
-        $period = Period::where('demurrage_id',$request->Triff_id)->select('rate','number_off_dayes','period')->get();
-        $demurrage = Demurrage::where('id',$request->Triff_id)->get();
-        $demurrages = Demurrage::where('company_id',Auth::user()->company_id)->get();
-        $containerType  = ContainersTypes::where('id',$movement->container_type_id)->pluck('name')->first();
-        $dchfDate = $request->dchfDate;
-        $rcvcDate = $request->rcvcDate;
-        $periodtimeTotal = 0;
-        foreach($period as $per){
-            if($per->period != "Thereafter")
-            $periodtimeTotal += $per->number_off_dayes;
-        }
-        
-        return view('containers.detention.index',[
-            'containerType'=>$containerType,
-            'container_no'=>$container_no,
-            'periodtimeTotal'=>$periodtimeTotal,
-            'dchfDate'=>$dchfDate,
-            'rcvcDate'=>$rcvcDate,
-            'movement'=>$movement,
-            'periods'=>$period,
-            'detention' => $detention,
-            'demurrage' => $demurrage,
-            'items'=>$demurrages,
-        ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        $this->authorize(__FUNCTION__,Demurrage::class);
+        $tariffTypes = TariffType::all();
+        $countries = Country::orderBy('id')->get();
+        $bounds = Bound::orderBy('id')->get();
+        $containersTypes = ContainersTypes::orderBy('id')->get();
+        $ports = Ports::orderBy('id')->where('company_id',Auth::user()->company_id)->get();
+        $triffs = Triff::get();
+        $currency = Currency::all();
+        $terminals = Terminals::where('company_id',Auth::user()->company_id)->get();
+        $containerstatus = ContainerStatus::orderBy('id')->get();
+        return view('containers.demurrage.create',[
+            'terminals'=>$terminals,
+            'countries'=>$countries,
+            'bounds'=>$bounds,
+            'containersTypes'=>$containersTypes,
+            'ports'=>$ports,
+            'triffs'=>$triffs,
+            'currency'=>$currency,
+            'containerstatus'=>$containerstatus,
+            'tariffTypes'=>$tariffTypes,
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $user = Auth::user();
+
+        $demurrages = Demurrage::create([
+            'country_id' => $request->input('country_id'),
+            'terminal_id' => $request->input('terminal_id'),
+//            'container_type_id'=> $request->input('container_type_id'),
+            'port_id' => $request->input('port_id'),
+            'validity_from' => $request->input('validity_from'),
+            'validity_to' => $request->input('validity_to'),
+            'currency' => $request->input('currency'),
+//            'bound_id'=> $request->input('bound_id'),
+//            'is_storge' => $request->is_storge,
+            'container_status' => $request->container_status,
+            'tariff_id' => $request->input('tariff_id'),
+            'company_id' => $user->company_id,
+            'tariff_type_id' => $request->tariff_type_id,
+        ]);
+        $slabs = collect($request->period)->groupBy('container_type');
+        foreach ($slabs as $slab) {
+            $createdSlab = DemuragePeriodsSlabs::create([
+                'demurage_id' => $demurrages->id,
+                'container_type_id' => $slab->first()['container_type']
+            ]);
+            foreach ($slab as $period) {
+                Period::create([
+                    'rate' => $period['rate'],
+                    'period' => $period['period'],
+                    'number_off_dayes' => $period['number_off_days'],
+                    'slab_id' => $createdSlab->id,
+                ]);
+            }
+        }
+        return redirect()->route('demurrage.index')->with('success', trans('Demurrage.created'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $this->authorize(__FUNCTION__,Demurrage::class);
+        $demurrages = Demurrage::find($id);
+        $slabs = DemuragePeriodsSlabs::where('demurage_id', $id)->with('periods')->get();
+
+        foreach ($slabs as $slab) {
+            foreach ($slab->periods as $period) {
+                $periodData = $period->toArray();
+            }
+        }
+            // dd($slabs);
+        return view('containers.demurrage.show',[
+            'demurrages'=>$demurrages,
+            'slabs'=>$slabs,
+            'periodData'=>$periodData,
+        ]);
     }
 
     /**
@@ -154,21 +124,53 @@ class DetentionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Demurrage $demurrage)
     {
-        //
+        $this->authorize(__FUNCTION__,Demurrage::class);
+        $user = Auth::user();
+        $demurrage = $demurrage->load('periods');
+        $countries = Country::orderBy('id')->get();
+        $bounds = Bound::orderBy('id')->get();
+        $containersTypes = ContainersTypes::orderBy('id')->get();
+        $ports = Ports::where('company_id',$user->company_id)->orderBy('id')->get();
+        $triffs = Triff::all();
+        $currency = Currency::all();
+        $terminals = Terminals::where('company_id',$user->company_id)->get();
+        $containerstatus = ContainerStatus::orderBy('id')->get();
+        return view('containers.demurrage.edit',[
+            'terminals'=>$terminals,
+            'demurrage' => $demurrage,
+            'countries'=>$countries,
+            'bounds'=>$bounds,
+            'containersTypes'=>$containersTypes,
+            'ports'=>$ports,
+            'triffs'=>$triffs,
+            'currency'=>$currency,
+            'containerstatus'=>$containerstatus,
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(Request $request, Demurrage $demurrage)
     {
-        //
+        $this->authorize(__FUNCTION__,Demurrage::class);
+        $demurrage = $demurrage->load('periods');
+        $input = [
+            'country_id' => $request->country_id,
+            'terminal_id' => $request->terminal_id,
+            'port_id' => $request->port_id,
+            'container_type_id' => $request->container_type_id,
+            'bound_id' => $request->bound_id,
+            'currency' => $request->currency,
+            'validity_from' => $request->validity_from,
+            'validity_to' => $request->validity_to,
+            'tariff_id' => $request->tariff_id,
+            'is_storge' => $request->is_storge,
+            'container_status' => $request->container_status,
+        ];
+        $demurrage->update($input);
+        $demurrage->createOrUpdatePeriod($request->period);
+        Period::destroy(explode(',',$request->removed));
+        return redirect()->route('demurrage.index')->with('success',trans('Demurrage.updated.success'));
     }
 
     /**
@@ -179,6 +181,15 @@ class DetentionController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $demurrage = Demurrage::find($id);
+        $slabs = DemuragePeriodsSlabs::where('demurage_id', $id)->with('periods')->get();
+        foreach ($slabs as $slab) {
+            foreach ($slab->periods as $period) {
+                $period->delete();
+            }
+            $slab->delete();
+        }
+        $demurrage->delete();
+        return redirect()->route('demurrage.index')->with('success', trans('Demurrage.deleted.success'));
     }
 }
